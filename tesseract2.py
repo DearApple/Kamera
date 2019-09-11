10150ee5bbb206584a9d41d2e3fd76c4652f3e8d
DearApple <32673454+DearApple@users.noreply.github.com>
2019-09-11 11:30:17 +0200

    Add files via upload

--- tesseract2.py
+++ tesseract2.py
@@ -2,26 +2,16 @@ from openalpr import Alpr
 
 from distutils.core import setup
 
-from distutils.core import setup
 
+from distutils.core import setup
 import datetime
 import sqlite3
-import cv2
-import time
-import base64
 
 from cam2 import Stream
 from Alpr_sc import AlprRecognition
 from zapis import SavePlateResult
 from CarData import CarData
 
-def stream_to_photo():
-		    cap = cv2.VideoCapture("rtsp://admin:admin123@192.168.2.240:554/ISAPI/Streaming/channels/102")
-		    _, frame = cap.read()
-		    succ, encode_frame = cv2.imencode('.jpg',frame)
-		  
-		    cap.release()			
-		    AlprRecognition().photo_convert(encode_frame)
 
 def write_base_data():
 			
@@ -47,7 +37,7 @@ try:
 	write_base_data()
 	while True:
         	
-		stream_to_photo()
+		Stream().stream_to_photo()
        
     
 		
